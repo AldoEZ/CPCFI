c468b36fbbeed5aa70d1b6ac1ee4e4f1013b6e4b
Aldo EZ <zacariasaldo.19@gmail.com>
2025-02-15 12:27:47 -0600

    Se agregan ejercicios

---
+++ practicas/ESCOM_Tercer-Invierno/A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()	
+#define pb push_back				
+#define sz(a) int(a.size())				
+#define F first
+#define S second
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pair<int,int>> vpii;
+typedef vector<pair<ll,ll>> vpll;
+const int MOD = 1e9+7;
+const int INF = INT_MAX;	
+const ll INF64 = LLONG_MAX;		
+const long double EPS = 1e-9;			
+const long double PI = acosl(-1.0L);
+
+void solve() {
+    int n; cin >> n;
+    vll a(n);
+    
+    for(int i = 0; i < n; i++) cin >> a[i];
+    
+    ll maxA = 0, ia;
+    for(int i = 0; i < n; i++) {
+        if(a[i] > 0) { maxA = a[i]; ia = i+1; break; }
+    }
+    if(maxA == 0) { cout << 0 << endl; return; }
+    ll sumT = 0;
+    
+    for(int i = ia; i <= n; i++) {
+        if((maxA > 0) == (a[i] > 0)) maxA = max(maxA, a[i]);
+        else {
+            sumT += maxA;
+            maxA = a[i];
+        }
+    }
+    sort(all(a));
+    if(a[n-1] > sumT) cout << a[n-1] << endl;
+    else cout << sumT << endl;
+}
+
+int main(){
+	fastIO();
+	int tc = 1;
+	//cin >> tc;
+	for (int t = 1; t <= tc; t++)solve();
+}
