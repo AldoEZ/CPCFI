a620a05001e7dae8e1c3d949b09155510d071524
AldoEZ <zacariasaldo.19@gmail.com>
2025-04-05 20:48:22 -0600

    dp

---
+++ practicas/DP/489B.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()	
+#define pb push_back				
+#define sz(a) int(a.size())				
+#define F first
+#define S second
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pair<int,int>> vpii;
+typedef vector<pair<ll,ll>> vpll;
+const int MOD = 1e9+7;
+const int INF = INT_MAX;	
+const ll INF64 = LLONG_MAX;		
+const long double EPS = 1e-9;			
+const long double PI = acosl(-1.0L);
+
+void solve() {
+    int n; cin >> n;
+    vi b(n);
+    for(int i = 0; i < n; i++) cin >> b[i];
+    
+    int m; cin >> m;
+    vi g(m);
+    for(int j = 0; j < m; j++) cin >> g[j];
+    
+    
+}
+
+int main(){
+	fastIO();
+	int tc = 1;
+	//cin >> tc;
+	for (int t = 1; t <= tc; t++)solve();
+}
